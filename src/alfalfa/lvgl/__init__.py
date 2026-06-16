88e0c4401ce6519e46fcae444a4bfe8f16cc33fb
Ricardo Marchesan <ricardo@azevem.com>
2026-06-16 02:52:38 +0200

    Add build config parameter for refresh period

--- src/alfalfa/lvgl/__init__.py
+++ src/alfalfa/lvgl/__init__.py
@@ -8,12 +8,19 @@ _cd = lobs.Path(__file__).parent
 _s = _cd / "src"
 
 
+class LvglOptions:
+    refresh_period = lobs.Parameter[float](default=0.033)
+    """The desired refresh period for the LVGL library, in seconds.
+    The default value is 0.033 seconds, which corresponds to a refresh rate of 30 Hz."""
+
+
 class Lvgl(
     lobs.Package,
     version=lobs.Version(9, 5, 0),
     tag="lvgl",
 ):
-    """LVGL package"""
+    options = LvglOptions()
+
     def _prepare_files(self, basepath: lobs.Path):
         _provider = lobs.providers.GitRepoPackage(
             "https://github.com/lvgl/lvgl",
@@ -30,9 +37,15 @@ class Lvgl(
         gen_path: lobs.Path,
     ) -> lobs.exporter.TCFG | None:
         flags: dict[str, t.Any] = {}
+        opts = self.options
+
+        if opts.refresh_period.value is not None:
+            flags["LV_DEF_REFR_PERIOD"] = int(opts.refresh_period.value * 1000)
+
         cfg = config_type()
         match cfg:
             case lobs.exporters.esp_idf.EspIdfConfig():
+                flags = {f"CONFIG_{k}": v for k, v in flags.items()}
                 cfg.config_flags.update(flags)
             case lobs.exporters.cmake.CmakeConfig():
                 pass
